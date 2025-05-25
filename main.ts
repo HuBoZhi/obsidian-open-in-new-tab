743802fc0767f30675582a8d296369d6e12d0ff1
HuBoZhi <1310225330@qq.com>
2025-05-25 10:43:48 +0800

    增加缓存优化

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import {around} from "monkey-around"
 export default class OpenInNewTabPlugin extends Plugin {
 	sameTabOnce: boolean; // 是否仅在当前标签页打开一次
 	uninstallMonkeyPatchOpenFile: (() => void) | null; // 卸载文件打开的猴子补丁
-	openedFileCache: {};
+	openedFileCache: { [key: string]: WorkspaceLeaf }; // 缓存已打开的文件
 
 	constructor(app: App, manifest: PluginManifest) {
 		super(app, manifest);
@@ -21,20 +21,14 @@ export default class OpenInNewTabPlugin extends Plugin {
 		console.log("loading " + this.manifest.name + " plugin");
 		this.monkeyPatchOpenFile(); // 添加文件打开的猴子补丁
 		this.addMenuItem(); // 添加右键菜单项
+		this.reloadCache(); // 重新加载缓存
+	}
 
-
-
-		// // 增加文件打开的监听
-		// this.registerEvent(
-		// 	this.app.workspace.on("file-open", (file) => {
-		//
-		// 		// 文件打开时将这个文件的 WorkspaceLeaf 对象存储到缓存中
-		// 		const leaf = this.app.workspace.getLeafByFile(file);
-		// 		if (leaf) {
-		// 			this.openedFileCache[file.path] = leaf;
-		// 		}
-		// 	})
-		// );
+	reloadCache() {
+		this.app.workspace.iterateAllLeaves((leaf) => {
+			const file = (leaf.view as any)?.file;
+			this.openedFileCache[file.path] = leaf;
+		})
 	}
 
 
@@ -63,14 +57,54 @@ export default class OpenInNewTabPlugin extends Plugin {
 
 	// 查找已存在的标签页
 	private findExistingLeaf(path: string): WorkspaceLeaf | null {
-		let searchLeaf = null;
+		// 检查缓存中是否存在已打开的文件
+		let existingLeaf = this.openedFileCache[path];
+		if (existingLeaf) {
+			return existingLeaf;
+		}
+
+		for (const leaf of this.app.workspace.getLeavesOfType("markdown")) {
+			const file = (leaf.view as any)?.file;
+			let tmpPath = null;
+			if (file === undefined) {
+				tmpPath = leaf.view.getState().file
+			} else {
+				tmpPath = file.path;
+			}
+			if (!tmpPath) {
+				continue;
+			}
+			if (!this.openedFileCache[tmpPath]) {
+				// 缓存已打开的文件
+				this.openedFileCache[tmpPath] = leaf;
+			}
+			if (tmpPath === path) {
+				// 如果找到已打开的文件，返回该标签页
+				existingLeaf = leaf;
+			}
+		}
+		if (existingLeaf) {
+			return existingLeaf;
+		}
+
 		this.app.workspace.iterateAllLeaves((leaf) => {
 			const file = (leaf.view as any)?.file;
-			if (file?.path === path) {
-				searchLeaf = leaf;
+			let tmpPath = null;
+			if (file === undefined) {
+				tmpPath = leaf.view.getState().file
+			} else {
+				tmpPath = file.path;
+			}
+			if (!this.openedFileCache[tmpPath]) {
+				// 缓存已打开的文件
+				this.openedFileCache[tmpPath] = leaf;
+			}
+			if (tmpPath === path) {
+				// 如果找到已打开的文件，返回该标签页
+				existingLeaf = leaf;
 			}
 		})
-		return searchLeaf;
+		return existingLeaf;
 	}
 
 	// 为文件打开功能添加猴子补丁
