3b1b7381f4e734ac4cbef6e8410478bcc484d34b
HuBoZhi <1310225330@qq.com>
2025-05-27 22:47:17 +0800

    feat-最大文档打开数量 开发完成待测试

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-import {App, Plugin, PluginManifest, TFile, WorkspaceLeaf} from "obsidian";
+import {App, Notice, Plugin, PluginManifest, PluginSettingTab, Setting, TFile, WorkspaceLeaf} from "obsidian";
 import {around} from "monkey-around"
 
 
+// 插件设置接口定义
+interface OpenInNewTabSettings {
+	maxOpenFileNum: number;
+}
+
+// 默认插件设置
+const DEFAULT_SETTINGS: OpenInNewTabSettings = {
+	maxOpenFileNum: 15
+};
+
 // 插件主类
 export default class OpenInNewTabPlugin extends Plugin {
+	settings: OpenInNewTabSettings;
 	sameTabOnce: boolean; // 是否仅在当前标签页打开一次
 	uninstallMonkeyPatchOpenFile: (() => void) | null; // 卸载文件打开的猴子补丁
 	openedFileCache: { [key: string]: WorkspaceLeaf }; // 缓存已打开的文件
+	lufQueueMaxSize: number;
+	lufQueue: Map<string, boolean>; // 使用 Map 保证有序性
 
 	constructor(app: App, manifest: PluginManifest) {
 		super(app, manifest);
@@ -14,11 +27,14 @@ export default class OpenInNewTabPlugin extends Plugin {
 		this.uninstallMonkeyPatchOpenFile = null;
 		// 初始化openedFileCache对象
 		this.openedFileCache = {};
+		this.lufQueueMaxSize = 200; // 队列的最大容量
+		this.lufQueue = new Map(); // 使用 Map 保证有序性
 	}
 
 	// 插件加载时调用
 	async onload() {
 		console.log("loading " + this.manifest.name + " plugin");
+		await this.loadSettings();
 		this.monkeyPatchOpenFile(); // 添加文件打开的猴子补丁
 		this.addMenuItem(); // 添加右键菜单项
 		this.reloadCache(); // 重新加载缓存
@@ -27,12 +43,22 @@ export default class OpenInNewTabPlugin extends Plugin {
 			this.reloadCache();
 		})
 
+		this.addSettingTab(new OpenInNewTabSettingTab(this.app, this)); // 添加设置面板
+	}
+
+	// 加载插件设置
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	// 保存插件设置
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	checkUn(o: any): boolean {
 		// 检查任意对象数组元素不为null,undefined
 		return o !== undefined && o !== null;
-
 	}
 
 	reloadCache() {
@@ -160,6 +186,35 @@ export default class OpenInNewTabPlugin extends Plugin {
 		return existingLeaf;
 	}
 
+	// 存储已打开的文件路径:添加或更新文件路径
+	lufCacheFilePathAdd(filePath: string) {
+		let needCloseFilePath = "";
+		// 如果文件路径不存在或为空，直接返回
+		if (!filePath || filePath.trim() === "") {
+			return;
+		}
+		if (this.lufQueue.has(filePath)) {
+			// 如果文件路径已存在，先删除旧的
+			this.lufQueue.delete(filePath);
+		} else if (this.lufQueue.size >= this.lufQueueMaxSize || this.lufQueue.size > this.settings.maxOpenFileNum) {
+			// 如果队列已满，移除最近最少使用的文件路径
+			const leastUsedKey = this.lufQueue.keys().next().value;
+			needCloseFilePath = leastUsedKey; // 记录需要关闭的文件路径
+			this.lufQueue.delete(leastUsedKey);
+		}
+		// 将文件路径添加到队列末尾
+		this.lufQueue.set(filePath, true);
+		if (needCloseFilePath !== "" && this.openedFileCache[needCloseFilePath]) {
+			const openedFileCacheElement = this.openedFileCache[needCloseFilePath];
+			if (openedFileCacheElement) {
+				// 如果需要关闭的文件路径在缓存中，关闭该标签页
+				openedFileCacheElement.detach();
+				delete this.openedFileCache[needCloseFilePath]; // 从缓存中删除
+			}
+		}
+	}
+
+
 	// 为文件打开功能添加猴子补丁
 	monkeyPatchOpenFile() {
 		// eslint-disable-next-line @typescript-eslint/no-this-alias
@@ -202,6 +257,7 @@ export default class OpenInNewTabPlugin extends Plugin {
 						return pluginInstance.app.workspace.revealLeaf(existingLeaf);
 						// 	setActiveLeaf
 					} else {
+						pluginInstance.lufCacheFilePathAdd(file.path);
 						// 在新标签页中打开文件
 						// pluginInstance.app.workspace.getLeaf("tab"): 获取一个新的标签页（tab）类型的 WorkspaceLeaf，用于在新标签页中打开文件。
 						// originalOpenFile.apply(...): 调用原始的 openFile 方法（被猴子补丁拦截的原始方法），并将新的标签页和文件信息作为参数传递。
@@ -214,5 +270,45 @@ export default class OpenInNewTabPlugin extends Plugin {
 				};
 			}
 		});
+	}
+}
+
+
+// 插件设置面板类
+class OpenInNewTabSettingTab extends PluginSettingTab {
+	plugin: OpenInNewTabPlugin;
+
+	constructor(app: any, plugin: OpenInNewTabPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	// 显示设置面板UI
+	display(): void {
+		const {containerEl} = this;
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: '扩展功能'});
+
+		new Setting(containerEl)
+			.setName('最多打开文档数')
+			.setDesc('设置最多打开文档数，如果文档数量超过此值，将按照最近最少使用策略删除最旧的文档')
+			.addText(text => text
+				.setValue(this.plugin.settings.maxOpenFileNum.toString())
+				.onChange(async (value) => {
+					// 检查输入值是否为数字
+					if (isNaN(parseInt(value))) {
+						new Notice("请输入一个数字");
+						return;
+					}
+					// 值不能大于100
+					if (parseInt(value) > 100) {
+						new Notice("最多只能打开100个文件");
+						return;
+					}
+
+					this.plugin.settings.maxOpenFileNum = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
 	}
 }
